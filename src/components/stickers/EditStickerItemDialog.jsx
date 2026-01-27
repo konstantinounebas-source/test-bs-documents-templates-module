94cd4e0acde681d6a3ae69b4dcdb2a8b55878092
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-01-27 19:10:19 +0000

    File changes

--- src/components/stickers/EditStickerItemDialog.jsx
+++ src/components/stickers/EditStickerItemDialog.jsx
@@ -75,11 +75,13 @@ export default function EditStickerItemDialog({ open, onClose, stickerItem, onSa
       const updateData = {
         status: "Needed",
         installed: false,
-        installed_date: "",
-        custody_status: "",
-        current_custodian_id: "",
-        installed_by: "",
-        need_reorder: false
+        installed_date: null,
+        custody_status: null,
+        current_custodian_id: null,
+        installed_by: null,
+        need_reorder: false,
+        reorder_reason: null,
+        reorder_date: null
       };
 
       await base44.entities.StickerItem.update(stickerItem.id, updateData);
