9ff5875c3fbb5afcf15168ef73ec1334c441e6af
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-02-06 01:05:13 +0000

    File changes

--- src/pages/MfgKPIDashboard.jsx
+++ src/pages/MfgKPIDashboard.jsx
@@ -6,7 +6,6 @@ import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@
 import { BarChart3, Lock } from "lucide-react";
 import MetricDefinitionManager from "@/components/manufacturing/metrics/MetricDefinitionManager";
 import DailyMetricValuesViewer from "@/components/manufacturing/metrics/DailyMetricValuesViewer";
-import MetricCalculationEngine from "@/components/manufacturing/metrics/MetricCalculationEngine";
 
 export default function MfgKPIDashboardPage() {
   const { data: kpiRuns = [] } = useQuery({
@@ -82,9 +81,6 @@ export default function MfgKPIDashboardPage() {
           <CardContent className="space-y-8">
             <div>
               <MetricDefinitionManager />
-            </div>
-            <div className="border-t pt-6">
-              <MetricCalculationEngine />
             </div>
             <div className="border-t pt-6">
               <DailyMetricValuesViewer />
