fa3f4b4cb84266c82c4b61dcd1b62598ecfa52d5
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-02-20 15:28:18 +0000

    File changes

--- src/components/manufacturing/daily/BatchHeaderTab.jsx
+++ src/components/manufacturing/daily/BatchHeaderTab.jsx
@@ -300,6 +300,7 @@ export default function BatchHeaderTab({ batchHeaders, selectedBatch, selectedDe
 
 
   const handleCreateBatch = (dateStr) => {
+    setSelectedDate(dateStr);
     setShowCreateDialog(true);
     setFormData({
       date: dateStr,
