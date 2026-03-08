896271404a146332e9fdc79f76578fed64c26244
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-03-08 15:37:53 +0000

    File changes

--- src/pages/MfgDailyProduction.jsx
+++ src/pages/MfgDailyProduction.jsx
@@ -270,33 +270,47 @@ export default function MfgDailyProduction() {
                       </TabsList>
 
                       <div className="mt-6">
-                        <TabsContent value="batch_lines">
-                          <BatchLinesTab batchId={selectedBatch?.id} department={selectedBatch?.department} selectedBundle={selectedBundle} />
-                        </TabsContent>
+                        {(activeTab === 'batch_lines' || loadedTabs.has('batch_lines')) && (
+                          <TabsContent value="batch_lines">
+                            <BatchLinesTab batchId={selectedBatch?.id} department={selectedBatch?.department} selectedBundle={selectedBundle} />
+                          </TabsContent>
+                        )}
 
-                        <TabsContent value="qc_initial">
-                          <QCInitialStockTab batchId={selectedBatch?.id} department={selectedBatch?.department} />
-                        </TabsContent>
+                        {(activeTab === 'qc_initial' || loadedTabs.has('qc_initial')) && (
+                          <TabsContent value="qc_initial">
+                            <QCInitialStockTab batchId={selectedBatch?.id} department={selectedBatch?.department} />
+                          </TabsContent>
+                        )}
 
-                        <TabsContent value="operations">
-                          <OperationsTab batchId={selectedBatch?.id} department={selectedBatch?.department} />
-                        </TabsContent>
+                        {(activeTab === 'operations' || loadedTabs.has('operations')) && (
+                          <TabsContent value="operations">
+                            <OperationsTab batchId={selectedBatch?.id} department={selectedBatch?.department} />
+                          </TabsContent>
+                        )}
 
-                        <TabsContent value="team_persons">
-                          <TeamTimePersonsTab batchId={selectedBatch?.id} />
-                        </TabsContent>
+                        {(activeTab === 'team_persons' || loadedTabs.has('team_persons')) && (
+                          <TabsContent value="team_persons">
+                            <TeamTimePersonsTab batchId={selectedBatch?.id} />
+                          </TabsContent>
+                        )}
 
-                        <TabsContent value="team_extra">
-                          <TeamTimeExtraTab batchId={selectedBatch?.id} />
-                        </TabsContent>
+                        {(activeTab === 'team_extra' || loadedTabs.has('team_extra')) && (
+                          <TabsContent value="team_extra">
+                            <TeamTimeExtraTab batchId={selectedBatch?.id} />
+                          </TabsContent>
+                        )}
 
-                        <TabsContent value="help_in">
-                          <HelpInTab batchId={selectedBatch?.id} department={selectedBatch?.department} />
-                        </TabsContent>
+                        {(activeTab === 'help_in' || loadedTabs.has('help_in')) && (
+                          <TabsContent value="help_in">
+                            <HelpInTab batchId={selectedBatch?.id} department={selectedBatch?.department} />
+                          </TabsContent>
+                        )}
 
-                        <TabsContent value="consumables">
-                          <ConsumablesActualTab batchId={selectedBatch?.id} />
-                        </TabsContent>
+                        {(activeTab === 'consumables' || loadedTabs.has('consumables')) && (
+                          <TabsContent value="consumables">
+                            <ConsumablesActualTab batchId={selectedBatch?.id} />
+                          </TabsContent>
+                        )}
                       </div>
                     </Tabs>
                   )}
