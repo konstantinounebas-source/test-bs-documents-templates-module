3275a501d2164675e5444fb5bc07ae1c225ecbd6
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-03-08 11:05:17 +0000

    File changes

--- src/components/manufacturing/standards/shared/exportToExcel.jsx
+++ src/components/manufacturing/standards/shared/exportToExcel.jsx
@@ -2,17 +2,17 @@
 import XLSX from 'xlsx';
 
 export async function exportDataTabToExcel(gridRows, operationColumns, bundleName) {
-  alert('Excel export is not available');
+  alert('Excel export is not not available');
 }
 
 export async function exportQCTabToExcel(filteredItems, gridData, mode, selectedOperation, selectedQCType, selectedQCLevel, bundleName) {
-  alert('Excel export is not available');
+  alert('Excel export is not not available');
 }
 
 export async function exportProfilesToExcel(profiles, operations, bundleName) {
-  alert('Excel export is not available');
+  alert('Excel export is not not available');
 }
 
 export async function exportScheduledDataToExcel(filteredLines, getProfileName, selectedDate, bundleName) {
-  alert('Excel export is not available');
+  alert('Excel export is not not available');
 }
