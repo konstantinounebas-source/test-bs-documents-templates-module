fdd4994cc439c1ef8840213807be6522487e670e
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-03-08 18:28:08 +0000

    File changes

--- src/components/templates/TemplateTable.jsx
+++ src/components/templates/TemplateTable.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import React, { useState } from "react";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
