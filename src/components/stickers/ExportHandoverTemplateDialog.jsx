ecc365a30aa3da77ed2f0721c881085ea12485f1
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-03-08 10:34:35 +0000

    Manual code change

--- src/components/stickers/ExportHandoverTemplateDialog.jsx
+++ src/components/stickers/ExportHandoverTemplateDialog.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { FileDown } from "lucide-react";
-import ExcelJS from 'exceljs';
+
 
 export default function ExportHandoverTemplateDialog({ availableItems, stops, stickerTemplates }) {
   const handleExportTemplate = async () => {
