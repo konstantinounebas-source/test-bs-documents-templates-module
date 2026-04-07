c4656b681622af61f8b1dae778c415dd7e739ca8
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-04-07 02:58:08 +0000

    File changes

--- src/components/manufacturing/daily/DailyFormsTab.jsx
+++ src/components/manufacturing/daily/DailyFormsTab.jsx
@@ -219,7 +219,7 @@ export default function DailyFormsTab({
         </div>
       </div>
 
-          {/* Attachments Area with Drag-Drop */}
+      {/* Attachments Area with Drag-Drop */}
       <div className="flex-1 flex flex-col min-h-0 px-4">
         {selectedDept ? (
           <>
