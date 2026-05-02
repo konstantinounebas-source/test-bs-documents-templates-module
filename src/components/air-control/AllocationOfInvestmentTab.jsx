408ee3fba6c4fb3aad0b23905a9bf23d4cd03ada
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-05-02 21:42:11 +0000

    File changes

--- src/components/air-control/AllocationOfInvestmentTab.jsx
+++ src/components/air-control/AllocationOfInvestmentTab.jsx
@@ -156,7 +156,7 @@ export default function AllocationOfInvestmentTab() {
                         </tr>
                         <tr>
                             <TD>Allocation %</TD>
-                            <CalcCell value={isNaN(allocationPct) ? 'N/A' : `${allocationPct.toFixed(2)} %`} className="text-right" />
+                            <CalcCell value={`${allocationPct.toFixed(2)} %`} className="text-right" />
                         </tr>
                         <tr className="bg-slate-100 font-bold">
                             <TD bold>Allocated Investment Cost</TD>
