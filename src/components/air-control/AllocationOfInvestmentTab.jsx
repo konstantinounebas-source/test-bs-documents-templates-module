9a934e85b5d986741b0e631ab4db162be7055d57
NIKOLAS KONSTANTINOU <konstantinoun.ebas@gmail.com>
2026-05-02 21:51:24 +0000

    Μορφοποίηση αριθμών με δεκαδικά

--- src/components/air-control/AllocationOfInvestmentTab.jsx
+++ src/components/air-control/AllocationOfInvestmentTab.jsx
@@ -60,10 +60,9 @@ export default function AllocationOfInvestmentTab() {
 
     const loadProjectData = async () => {
         try {
-            // Load ProjectMasterData
-            const pmRecords = await base44.entities.ProjectMasterData.list();
-            if (pmRecords.length > 0) {
-                const record = pmRecords[0];
+            const records = await base44.entities.ProjectMasterData.list();
+            if (records.length > 0) {
+                const record = records[0];
                 const projectBudgetData = record.project_budget_correction || {};
                 const fabricationData = record.fabrication_budget || {};
                 
@@ -94,19 +93,6 @@ export default function AllocationOfInvestmentTab() {
                     ...prev,
                     expected_income: totalIncome || prev.expected_income
                 }));
-            }
-            
-            // Load IncomeCalculation to get totalValueOfWorkPerformed
-            const incomeRecords = await base44.entities.IncomeCalculation.list();
-            if (incomeRecords.length > 0) {
-                const incomeMap = {};
-                incomeRecords.forEach(r => { incomeMap[r.section] = r; });
-                
-                // Get the section that contains Value of Work Performed calculation
-                // Based on the Income tab, this is calculated as: totalIncomeReceived + totalIncomeNotEarned - advancePaymentRemaining
-                // We'll store a placeholder and the component will calculate it from the displayed values
-                // Or if stored, retrieve it from the database
-                // For now, we'll rely on the manual input field, but we can enhance this later
             }
         } catch (error) {
             console.error('Failed to load project data:', error);
@@ -162,7 +148,7 @@ export default function AllocationOfInvestmentTab() {
                         </tr>
                         <tr>
                             <TD>Total Value of Work Performed</TD>
-                            <CalcCell value={parseNum(investment.total_value_work)} />
+                            <InputCell value={fmt(investment.total_value_work)} onChange={v => setInvestment({...investment, total_value_work: parseNum(v)})} />
                         </tr>
                         <tr>
                             <TD>Expected Total Project Income</TD>
